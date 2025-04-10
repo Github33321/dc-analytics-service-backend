fecf523417103c22ad449e630058cf9fc31f748c
Maxim Shamochkin <maxim.shamochkin@cyberzion.ru>
2025-04-10 19:51:47 +0300

    docker3

--- internal/config/config.go
+++ internal/config/config.go
@@ -23,17 +23,17 @@ type Config struct {
 }
 
 func NewConfig() (*Config, error) {
-	if err := godotenv.Load("/opt/dc-analytics-service/.env"); err != nil {
+	if err := godotenv.Load("/dc-analytics-service-backend/.env"); err != nil {
 		fmt.Println("Не удалось загрузить файл .env, используются переменные окружения из системы")
 	}
 	debug := getEnv("CLICKHOUSE_DEBUG", "false")
 	return &Config{
-		Port:      getEnv("PORT", "8081"),
+		Port:      getEnv("PORT", "7002"),
 		JWTSecret: getEnv("JWT_SECRET", "default_super_secret"),
 		LogLevel:  getEnv("LOG_LEVEL", "INFO"),
 		ClickhouseConfig: &ClickhouseConfig{
 			Host:     getEnv("CLICKHOUSE_HOST", "localhost:9000"),
-			Database: getEnv("CLICKHOUSE_DB", "default"),
+			Database: getEnv("CLICKHOUSE_DB", "device_cloud_webhooks"),
 			Username: getEnv("CLICKHOUSE_USERNAME", "default"),
 			Password: getEnv("CLICKHOUSE_PASSWORD", "default"),
 			Debug:    debug == "true",
